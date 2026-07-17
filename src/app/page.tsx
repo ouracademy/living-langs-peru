57db5aee4828cdb7667e97fbd14f399c68f496f4
Dede1421 <mauricioandre91@gmail.com>
2026-07-16 22:40:53 -0500

    style: add a logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
-
 import { Button, buttonVariants } from "@/components/ui/button";
 import { languages } from "@/lib/languages";
 import { cn } from "@/lib/utils";
-import { Globe, User } from "lucide-react";
+import { User } from "lucide-react";
+import Image from "next/image";
+
 
 const menuItems = [
   { label: "Languages & Resources", href: "/languages-resources" },
@@ -18,23 +19,28 @@ export default function Home() {
       <header className="flex sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
         <div className="container mx-auto flex h-16 items-center justify-between px-4">
           <Link href="/" className="flex items-center gap-2 font-bold text-lg">
-            {/* <div> */}
-            <Globe className="h-5 w-5" />
-            {/* </div> */}
+            <Image
+              src="/logo.webp"
+              alt="lingua plataform"
+              width={100}
+              height={100}
+              style={{ height: "auto" }}
+              className="object-contain"
+            />
             <span>Lingua Plataform</span>
           </Link>
-        
-        <nav className="hidden md:flex items-center gap-6">
-          {menuItems.map((item) => (
-            <Link href={item.href} key={item.label} className="text-base font-medium text-muted-foreground  hover:text-[#044D58] transition-colors">
-              {item.label}
-            </Link>
-          ))}
-        </nav>
-        <Button variant="ghost" size="icon" className="rounded-full border h-9 w-9">
-          <User className="h-4.5 w-4.5 text-muted-foreground" />
-          <span className="sr-only font-normal">Usuario</span>
-        </Button>
+
+          <nav className="hidden md:flex items-center gap-6">
+            {menuItems.map((item) => (
+              <Link href={item.href} key={item.label} className="text-base font-medium text-muted-foreground  hover:text-[#044D58] transition-colors">
+                {item.label}
+              </Link>
+            ))}
+          </nav>
+          <Button variant="ghost" size="icon" className="rounded-full border h-9 w-9">
+            <User className="h-4.5 w-4.5 text-muted-foreground" />
+            <span className="sr-only font-normal">Usuario</span>
+          </Button>
         </div>
       </header>
       <main className="flex flex-1 flex-col">
