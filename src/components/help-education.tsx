2595721d3558b486b9b8758fa99c891844a79f06
Dede1421 <mauricioandre91@gmail.com>
2026-07-31 18:23:38 -0500

    style: add icons in helpEducation section

--- src/components/help-education.tsx
+++ src/components/help-education.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
 import { Button } from "./ui/button"
-import { Card, CardFooter, CardHeader, CardTitle } from "./ui/card"
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
+import { BookOpen, FolderOpen, Gamepad2 } from "lucide-react"
+
 
 const educationI = [
   {
@@ -8,15 +10,23 @@ const educationI = [
     description: "",
     buttonText: "Ver tutoriales",
     href:"/tutoriales",
-    icon: "",
+    icon: BookOpen,
   }, 
   {
     title: "Games",
     description: "",
-    buttonText: "abc",
+    buttonText: "Aprende jugando",
     href: "/juegos",
-    icon: "",
+    icon: Gamepad2,
+  },
+  {
+    title:"Materiales y videos",
+    description:"",
+    buttonText: "Explorar recursos",
+    href:"/materiales",
+    icon: FolderOpen,
   }
+
 ]
 
 
@@ -26,15 +36,21 @@ export function HelpEducation () {
         <div className="mx-auto max-w-6xl text-center">
           <h2 className="text-3xl tracking-tight text-foreground">Centro de educación y ayuda</h2>
           <p className="mt-3 text-muted-foreground text-lg">Recursos, herramientas interactivas y guías</p>
-          <div className="mt-12 grid grid-col-1 gap-8 md:grid-cols-3">
+          
+          <div className="mt-12 grid grid-cols-1 gap-8 md:grid-cols-3">
             {educationI.map((item)=> {
+              const Icon = item.icon;
               return (
                 <Card key={item.title}>
                   <CardHeader>
-                    <CardTitle>{item.title}</CardTitle>
+                    <div className=" mb-3 flex justify-center items-center h-12 w-12 bg-primary/10 rounded-lg">
+                      <Icon className="h-6 w-6"/>
+                    </div>
+                    <CardTitle className="text-xl">{item.title}</CardTitle>
+                    <CardDescription className="pt-2">{item.description}</CardDescription>
                   </CardHeader>
-                  <CardFooter>
-                    <Button variant="outline" className="w-full">
+                  <CardFooter className="pt-4">
+                    <Button className="w-full">
                       <Link href={item.href}>{item.buttonText}</Link>
                     </Button>
                   </CardFooter>
