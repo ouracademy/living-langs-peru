e727123c3d0faa7b8f68d8ab66ac044138b517f7
Dede1421 <mauricioandre91@gmail.com>
2026-08-14 17:22:34 -0500

    style: add responsive header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,21 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { User } from "lucide-react";
+import { Menu, User } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import React from "react";
 
 const menuItems = [
   { label: "Lenguas & Recursos", href: "/languages-resources" },
   { label: "Eventos", href: "/events" },
   { label: "Historias", href: "/stories" },
   { label: "Involucrate", href: "/get-involved" },
-]
+];
 
 export function HeaderPlataform() {
+  const [ isOpen, setIsOpen] = React.useState(false)
   return (
     <header className="flex sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
@@ -19,7 +24,7 @@ export function HeaderPlataform() {
             src="/logo.webp"
             alt="Lenguas Peruanas"
             width={100}
-            height={100}            
+            height={100}
             className="object-contain h-auto w-24"
           />
           <span>Lenguas Peruanas</span>
@@ -27,16 +32,46 @@ export function HeaderPlataform() {
 
         <nav className="hidden md:flex items-center gap-6">
           {menuItems.map((item) => (
-            <Link href={item.href} key={item.label} className="text-base font-medium text-muted-foreground  hover:text-[#044D58] transition-colors">
+            <Link
+              href={item.href}
+              key={item.label}
+              className="text-base font-medium text-muted-foreground  hover:text-[#044D58] transition-colors"
+            >
               {item.label}
             </Link>
           ))}
         </nav>
-        <Button variant="ghost" size="icon" className="rounded-full border h-9 w-9">
-          <User className="h-4.5 w-4.5 text-muted-foreground" />
-          <span className="sr-only font-normal">Usuario</span>
-        </Button>
+        {/* Usuario y Menu sandwich */}
+        <div className="flex items-center gap-2">
+          {/*User button */}
+          <Button
+            variant="ghost"
+            size="icon"
+            className="rounded-full border h-9 w-9"
+          >
+            <User className="h-4.5 w-4.5 text-muted-foreground" />
+            <span className="sr-only font-normal">Usuario</span>
+          </Button>
+          {/* Menu sandwich*/}
+          <div className="md:hidden">
+            <Sheet open={isOpen} onOpenChange={setIsOpen}>
+              <SheetTrigger>
+                <Button variant="outline" size="icon">
+                  <Menu className="h-5 w-5"/>
+                  <span className="sr-only">Abrir menú</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="left"  className="">
+                <SheetHeader>
+                  <SheetTitle className="text-left text-lg">
+                    Lenguas Peruanas
+                  </SheetTitle>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </div>
       </div>
     </header>
-  )
+  );
 }
